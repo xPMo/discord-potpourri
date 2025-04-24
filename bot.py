d5d3d7c01e9b6b9a92b07c73adb0f88a81aac8ff
gammafn <GammaFunction@vivaldi.net>
2025-04-24 09:44:30 -0500

    Split Rivals 2 into its own cog file

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import random
 import scrape.dragdown
 from bs4 import BeautifulSoup
 from discord.commands import option
+from discord.ext import commands
 from dotenv import load_dotenv
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
@@ -17,6 +18,8 @@ token = os.environ['BOT_TOKEN']
 intents = discord.Intents.default()
 bot = discord.Bot()
 bot.default_command_integration_types.add(discord.IntegrationType.user_install)
+
+bot.load_extension('rivals2')
 
 def prefix_match_key(prefix, dictionary):
     '''
@@ -39,50 +42,5 @@ async def ping(ctx):
     logging.debug(f'{ctx.command}: {ctx.user}')
     logging.debug(f'{ctx.command}: {ctx.guild} ({ctx.guild_id}) {ctx.channel} ({ctx.channel_id})')
     await ctx.respond(f'**Pong!** ({round(1000 * bot.latency, 1)}ms)')
-
-rivals2characters = scrape.dragdown.characterlist()
-
-def rivals2_palette_complete(ctx: discord.AutocompleteContext):
-    char = ctx.options['character']
-    skin = ctx.options['skin']
-    try:
-        return [k for k in rivals2characters[char].skins[skin].keys()
-                if k.lower().startswith(ctx.value.lower())]
-    except:
-        return []
-
-def rivals2_skin_complete(ctx: discord.AutocompleteContext):
-    char = ctx.options['character']
-    try:
-        return [k for k in rivals2characters[char].skins.keys()
-                if k.lower().startswith(ctx.value.lower())]
-    except:
-        return []
-
-@bot.slash_command(name='palette', description='Get a Rivals 2 palette')
-@option('character', description='Rivals 2 Character',
-        autocomplete=discord.utils.basic_autocomplete(rivals2characters.keys())
-)
-@option('skin', description='Choose a skin',
-        autocomplete=rivals2_skin_complete
-)
-@option('palette', description='Choose a palette',
-        autocomplete=rivals2_palette_complete
-)
-async def r2pallete(ctx, character: str, skin: str, palette: str):
-    logging.debug(f'{ctx.command}: {ctx.user}')
-    logging.debug(f'{ctx.command}: {ctx.guild} ({ctx.guild_id}) {ctx.channel} ({ctx.channel_id})')
-    try:
-        full, thumb, unlock = rivals2characters[character].skins[skin][palette]
-        embed = discord.Embed(title=f'{skin} {character} ({palette})')
-        embed.url = scrape.dragdown.DOMAIN + full
-        embed.set_image(url='https:' + thumb)
-        embed.set_footer(text=unlock)
-        await ctx.respond(None, embed=embed)
-    except KeyError as e:
-        logging.info(f'{ctx.command}: No {character}/{skin}/{palette}', exc_info=e)
-        await ctx.respond(f'Could not find {e} for {character}/{skin}/{palette}')
-# Not ready yet
-#@bot.slash_command(name='deck', description='Draw from a deck of cards')
 
 bot.run(token)
---
+++ rivals2.py
@@ -0,0 +1,76 @@
+#!python
+import discord
+import logging
+import scrape.dragdown
+from discord.commands import option
+from discord.ext import commands
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
+
+class Completions:
+    def __init__(self, characters):
+        self.characters = characters
+
+    def palettes(self, ctx: discord.AutocompleteContext):
+        char = ctx.options['character']
+        skin = ctx.options['skin']
+        try:
+            return [k for k in characters[char].skins[skin].keys()
+                    if k.lower().startswith(ctx.value.lower())]
+        except:
+            return []
+
+    def skins(self, ctx: discord.AutocompleteContext):
+        char = ctx.options['character']
+        try:
+            return [k for k in characters[char].skins.keys()
+                    if k.lower().startswith(ctx.value.lower())]
+        except:
+            return []
+
+
+# have to create completions object first
+characters = scrape.dragdown.characterlist()
+completions = Completions(characters)
+
+class Cog(commands.Cog):
+
+    def __init__(self, bot, characters):
+        logging.debug("Loading Rivals 2 Cog")
+        self.bot = bot
+
+    @commands.slash_command(name='resetcharacters', description='Reload all data for Rivals 2 characters')
+    async def resetcharacters(self, ctx):
+        logging.debug(f'{ctx.command}: {ctx.user}')
+        logging.debug(f'{ctx.command}: {ctx.guild} ({ctx.guild_id}) {ctx.channel} ({ctx.channel_id})')
+
+        new = scrape.dragdown.characterlist()
+        characters.update(new)
+        await ctx.respond('Reset!')
+
+    @commands.slash_command(name='palette', description='Get a Rivals 2 palette')
+    @option('character', description='Rivals 2 Character',
+            autocomplete=discord.utils.basic_autocomplete(characters.keys())
+    )
+    @option('skin', description='Choose a skin',
+            autocomplete=completions.skins
+    )
+    @option('palette', description='Choose a palette',
+            autocomplete=completions.palettes
+    )
+    async def palette(self, ctx, character: str, skin: str, palette: str):
+        logging.debug(f'{ctx.command}: {ctx.user}')
+        logging.debug(f'{ctx.command}: {ctx.guild} ({ctx.guild_id}) {ctx.channel} ({ctx.channel_id})')
+        try:
+            full, thumb, unlock = characters[character].skins[skin][palette]
+            embed = discord.Embed(title=f'{skin} {character} ({palette})')
+            embed.url = scrape.dragdown.DOMAIN + full
+            embed.set_image(url='https:' + thumb)
+            embed.set_footer(text=unlock)
+            await ctx.respond(None, embed=embed)
+        except KeyError as e:
+            logging.info(f'{ctx.command}: No {character}/{skin}/{palette}', exc_info=e)
+            await ctx.respond(f'Could not find {e} for {character}/{skin}/{palette}')
+
+def setup(bot):
+    bot.add_cog(Cog(bot, characters))
